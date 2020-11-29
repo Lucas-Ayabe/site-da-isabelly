19b9104777d004b490ea223d012612e8e3a8d966
Lucas Akira Ayabe <lucas.ayabe@gmail.com>
2020-11-29 04:34:49 -0300

    refactored js to more functional way

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
+const curry = (fn, ...args) => {
+    return fn.length <= args.length
+        ? fn(...args)
+        : curry.bind(null, fn, ...args);
+};
+
 const select = (selectors) => document.querySelector(selectors);
 const selectAll = (selectors) => document.querySelectorAll(selectors);
+const onEvent = curry((event) => (element) => (callback) => {
+    return element.addEventListener(event, callback);
+});
+
+const onScroll = onEvent('scroll');
+const onClick = onEvent('click');
 
 const navigation = select('.navigation');
 const hamburger = select('.hamburger');
@@ -9,24 +21,31 @@ const themeToggler = select('#theme-toggler[data-theme]');
 const navigationFloatingClass = '-floating';
 const menuOpenClass = '-open';
 
-window.addEventListener('scroll', () => {
-    console.log(window.pageYOffset);
+const onScrollInWindow = onScroll(window);
+const onClickInThemeToggler = onClick(themeToggler);
+const onClickInHamburger = onClick(hamburger);
+
+function toggleMenuIfScrolled() {
     if (window.pageYOffset > 30) {
         navigation.classList.add(navigationFloatingClass);
     } else {
         navigation.classList.remove(navigationFloatingClass);
     }
-});
+}
 
-themeToggler.addEventListener('click', () => {
+function toggleTheme() {
     const theme = themeToggler.dataset.theme;
     const newTheme = theme === 'light' ? 'dark' : 'light';
     themeToggler.dataset.theme = newTheme;
     themeToggler.innerHTML = newTheme;
     document.documentElement.classList.toggle('dark-theme');
-});
+}
 
-hamburger.addEventListener('click', () => {
+function toggleMenu() {
     hamburger.classList.toggle(menuOpenClass);
     menu.classList.toggle(menuOpenClass);
-});
+}
+
+onScrollInWindow(toggleMenuIfScrolled);
+onClickInThemeToggler(toggleTheme);
+onClickInHamburger(toggleMenu);
