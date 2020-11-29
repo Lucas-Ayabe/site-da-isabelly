2965e5202a6e13f091b17e2d99d35f3f5bf25ecd
Lucas Akira Ayabe <lucas.ayabe@gmail.com>
2020-11-29 04:19:08 -0300

    added menu and navibar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
 const select = (selectors) => document.querySelector(selectors);
 const selectAll = (selectors) => document.querySelectorAll(selectors);
 
+const navigation = select('.navigation');
+const hamburger = select('.hamburger');
+const menu = select('.menu');
 const themeToggler = select('#theme-toggler[data-theme]');
+
+const navigationFloatingClass = '-floating';
+const menuOpenClass = '-open';
+
+window.addEventListener('scroll', () => {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > 30) {
+        navigation.classList.add(navigationFloatingClass);
+    } else {
+        navigation.classList.remove(navigationFloatingClass);
+    }
+});
 
 themeToggler.addEventListener('click', () => {
     const theme = themeToggler.dataset.theme;
@@ -9,4 +24,9 @@ themeToggler.addEventListener('click', () => {
     themeToggler.dataset.theme = newTheme;
     themeToggler.innerHTML = newTheme;
     document.documentElement.classList.toggle('dark-theme');
+});
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle(menuOpenClass);
+    menu.classList.toggle(menuOpenClass);
 });
